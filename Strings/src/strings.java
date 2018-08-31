6db6a7a8a4f4ba01dd4bdb014cccd80460310a3f
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-08-31 08:38:36 -0600

    finished string challenge 1 and 2

--- Strings/src/strings.java
+++ Strings/src/strings.java
@@ -1,11 +1,31 @@
-
+import java.util.Scanner;
 public class strings
 	{
-
+		static Scanner userInput = new Scanner(System.in);
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
-
+				//quiteDown();
+				//printVerticalLetters();
 			}
-
+		public static void quiteDown() 
+			{
+				System.out.println("Yell something into the computer");
+				String yelling = userInput.nextLine();
+				System.out.print(yelling.substring(0,1));
+				System.out.print(yelling.substring(1, yelling.length()).toLowerCase());
+			}
+		public static void printVerticalLetters()
+			{
+				System.out.println("Type in a word");
+				String word = userInput.nextLine();
+				for (int i = 0; i < word.length(); i++)
+					{
+						System.out.println(word.substring(i, i+1));
+					}
+			}
+		public static void findVowels()
+			{
+				System.out.println("Type a word to find the number of vowels");
+				
+			}
 	}
